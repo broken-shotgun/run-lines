5bb5c6719994c591066bf6035fac46956320c97a
Jason Petterson <jpetterson@detroitlabs.com>
2020-03-06 14:46:10 -0500

    comment out debug logging statements

--- app/src/main/java/com/brokenshotgun/runlines/data/FountainSerializer.kt
+++ app/src/main/java/com/brokenshotgun/runlines/data/FountainSerializer.kt
@@ -146,7 +146,7 @@ object FountainSerializer {
         val titleTokens = parseTitlePageOfString(getScriptTitlePage(script))
         val bodyTokens = parseBodyOfString(getScriptBody(script))
 
-        //*
+        /*
         // for debugging only
         println("##### all tokens for Script #####")
         println(titleTokens)
@@ -157,7 +157,7 @@ object FountainSerializer {
                     "sectionDepth=${token.sectionDepth}]\t" +
                     "<${token.elementType}>${token.elementText}</${token.elementType}>")
         }
-        //*/
+        */
 
         return Script(titleTokens, bodyTokens)
     }
