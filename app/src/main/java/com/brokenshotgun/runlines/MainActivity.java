15bc08c72729160c7dd39765600940744c955384
JP <jp@brokenshotgun.com>
2026-09-13 15:02:28 -0400

    arhived manual attempt to update

--- app/src/main/java/com/brokenshotgun/runlines/MainActivity.java
+++ app/src/main/java/com/brokenshotgun/runlines/MainActivity.java
@@ -55,6 +55,7 @@ import com.tom_roush.pdfbox.text.PDFTextStripper;
 import com.tom_roush.pdfbox.util.PDFBoxResourceLoader;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -73,31 +74,25 @@ public class MainActivity extends AppCompatActivity {
         void onFailure();
     }
     private ProgressDialog progressDialog;
-    private ImportCallback importScriptHandler = new ImportCallback() {
+    private final ImportCallback importScriptHandler = new ImportCallback() {
         @Override
         public void onSuccess(final Script script) {
             dbHelper.insertScript(script);
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    progressDialog.dismiss();
-                    progressDialog = null;
-                    scriptListAdapter.add(script);
-                    Snackbar.make(scriptListView, R.string.alert_script_import_success, Snackbar.LENGTH_LONG).show();
-                }
+            runOnUiThread(() -> {
+                progressDialog.dismiss();
+                progressDialog = null;
+                scriptListAdapter.add(script);
+                Snackbar.make(scriptListView, R.string.alert_script_import_success, Snackbar.LENGTH_LONG).show();
             });
 
         }
 
         @Override
         public void onFailure() {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    progressDialog.dismiss();
-                    progressDialog = null;
-                    Snackbar.make(scriptListView, R.string.alert_script_import_error, Snackbar.LENGTH_LONG).show();
-                }
+            runOnUiThread(() -> {
+                progressDialog.dismiss();
+                progressDialog = null;
+                Snackbar.make(scriptListView, R.string.alert_script_import_error, Snackbar.LENGTH_LONG).show();
             });
         }
     };
@@ -142,39 +137,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        importScriptButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showImportFileSelect();
-            }
-        });
+        importScriptButton.setOnClickListener(view -> showImportFileSelect());
 
         dbHelper = new ScriptReaderDbHelper(this);
 
-        scriptListAdapter = new ScriptArrayAdapter(this, new ArrayList<Script>());
+        scriptListAdapter = new ScriptArrayAdapter(this, new ArrayList<>());
         scriptListView.setAdapter(scriptListAdapter);
         scriptListView.setEmptyView(findViewById(android.R.id.empty));
-        scriptListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                openScript(scriptListAdapter.getItem(position));
-            }
+        scriptListView.setOnItemClickListener((parent, view, position, id) -> openScript(scriptListAdapter.getItem(position)));
+
+        scriptListView.setOnItemLongClickListener((parent, view, position, id) -> {
+            showEditScriptDialog(scriptListAdapter.getItem(position), position);
+            return true;
         });
 
-        scriptListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                showEditScriptDialog(scriptListAdapter.getItem(position), position);
-                return true;
-            }
-        });
-
-        addScriptButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onAddScriptButtonClicked(v);
-            }
-        });
+        addScriptButton.setOnClickListener(this::onAddScriptButtonClicked);
 
         if (savedInstanceState == null) {
             Intent intent = getIntent();
@@ -344,61 +321,75 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void importScriptFromText(final Uri filename, final ImportCallback importCallback) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String content = fileToString(filename);
-                if (content == null) {
-                    if (importCallback != null) {
-                        importCallback.onFailure();
-                    }
-                    return;
-                }
-                Script result = FountainSerializer.deserialize(content);
+        new Thread(() -> {
+            String content = fileToString(filename);
+            if (content == null) {
                 if (importCallback != null) {
-                    importCallback.onSuccess(result);
+                    importCallback.onFailure();
                 }
+                return;
+            }
+            Script result = FountainSerializer.deserialize(content);
+            if (importCallback != null) {
+                importCallback.onSuccess(result);
             }
         }).start();
     }
 
     protected void importScriptFromPdf(final Uri filename, final ImportCallback importCallback) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                PDDocument document = null;
-                try {
-                    InputStream fileStream = getContentResolver().openInputStream(filename);
-                    if (fileStream == null) {
-                        Log.e(MainActivity.class.getName(), "Could not open file, InputStream is null");
-                        if (importCallback != null)
-                            importCallback.onFailure();
-                        return;
-                    }
-                    document = PDDocument.load(fileStream, MemoryUsageSetting.setupTempFileOnly());
-                    PDFTextStripper textStripper = new PDFTextStripper();
-                    textStripper.setAddMoreFormatting(true);
-                    textStripper.setLineSeparator("\n");
-                    textStripper.setPageEnd("");
-                    String content = textStripper.getText(document);
-                    Script result = PdfParser.parse(content);
-                    if (importCallback != null)
-                        importCallback.onSuccess(result);
-                } catch (IOException e) {
-                    Log.e(MainActivity.class.getName(), e.getMessage(), e);
+        new Thread(() -> {
+            PDDocument document = null;
+            try {
+                InputStream fileStream = getContentResolver().openInputStream(filename);
+                if (fileStream == null) {
+                    Log.e(MainActivity.class.getName(), "Could not open file, InputStream is null");
                     if (importCallback != null)
                         importCallback.onFailure();
-                } finally {
-                    if (document != null) {
-                        try {
-                            document.close();
-                        } catch (IOException e) {
-                            Log.e(MainActivity.class.getName(), e.getMessage(), e);
-                        }
+                    return;
+                }
+                document = PDDocument.load(fileStream, MemoryUsageSetting.setupTempFileOnly());
+                PDFTextStripper textStripper = new PDFTextStripper();
+                textStripper.setAddMoreFormatting(true);
+                textStripper.setLineSeparator("\n");
+                textStripper.setPageEnd("");
+                String content = textStripper.getText(document);
+                Script result = PdfParser.parse(content);
+                if (importCallback != null)
+                    importCallback.onSuccess(result);
+            } catch (IOException e) {
+                Log.e(MainActivity.class.getName(), e.getMessage(), e);
+                if (importCallback != null)
+                    importCallback.onFailure();
+            } finally {
+                if (document != null) {
+                    try {
+                        document.close();
+                    } catch (IOException e) {
+                        Log.e(MainActivity.class.getName(), e.getMessage(), e);
                     }
                 }
             }
         }).start();
+    }
+
+    public static final int MAX_FILE_SIZE_BYTES = 20 * 1000000;
+    private boolean isFileTooBig(Intent data) {
+        Uri filePath = data.getData();
+        int fileSize = 0;
+        if (data.getData().getScheme().equals("file")) {
+            File file = new File(filePath.toString());
+            fileSize = (int) file.length();
+            Log.d(MainActivity.class.getName(), "This is the file size: " + fileSize);
+        } else if (data.getData().getScheme().equals("content")) {
+            try (Cursor returnCursor = this.getContentResolver().query(filePath, null, null, null, null)) {
+                assert returnCursor != null;
+                int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+                returnCursor.moveToFirst();
+                fileSize = returnCursor.getInt(sizeIndex);
+            }
+            System.out.println("This is the file size: " + fileSize);
+        }
+        return fileSize >= MAX_FILE_SIZE_BYTES;
     }
 
     protected String fileToString(Uri filename) {
@@ -437,6 +428,12 @@ public class MainActivity extends AppCompatActivity {
                 if (fileUri == null) {
                     Log.e(MainActivity.class.getName(), "File URI is null");
                     Snackbar.make(scriptListView, R.string.alert_error_file_open, Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (isFileTooBig(data)) {
+                    Log.e(MainActivity.class.getName(), "File greater than 20MB");
+                    Snackbar.make(scriptListView, R.string.alert_error_file_too_big, Snackbar.LENGTH_SHORT).show();
                     return;
                 }
 
